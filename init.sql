1143191c9b226463849eca01a948be2aeca59d58
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2024-03-19 18:48:55 -0300

    refactor: delete postgresql.conf

---
+++ init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    limite INTEGER NOT NULL,
+    saldo INTEGER NOT NULL DEFAULT 0
+);
+INSERT INTO clientes (nome, limite)
+VALUES 
+('João', 1000),
+('Maria', 2000),
+('José', 3000),
+('Ana', 4000),
+('Carlos', 5000);
