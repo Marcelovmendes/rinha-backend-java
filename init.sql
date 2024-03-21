e1bf1cd80ce661476deab05dba1650a0af9af49d
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2024-03-20 22:14:36 -0300

    feat: add creation entity in sql script

--- init.sql
+++ init.sql
@@ -4,6 +4,17 @@ CREATE TABLE IF NOT EXISTS clientes (
     limite INTEGER NOT NULL,
     saldo INTEGER NOT NULL DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS transacoes (
+    id SERIAL PRIMARY KEY,
+    valor INTEGER NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    descricao VARCHAR(250) NOT NULL,
+    id_cliente INTEGER NOT NULL,
+    realizado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id)
+);
+
 INSERT INTO clientes (nome, limite)
 VALUES 
 ('João', 1000),
